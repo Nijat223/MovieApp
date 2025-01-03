215b8b2ac3cc2ee56ce766c497e38989a454b931
Nijat223 <snijat@icloud.com>
2025-01-03 20:36:30 +0400

    integrated NSCollectionLayoutSection

---
+++ MovieApp/Presentation/HomeTabBar/Home/Views/SectionHeader.swift
@@ -0,0 +1,35 @@
+//
+//  SectionHeader.swift
+//  MovieApp
+//
+//  Created by Nijat Shikhaliyev on 03.01.25.
+//
+
+import UIKit
+
+final class SectionHeader: UICollectionReusableView {
+    
+    private lazy var divider: UIView = {
+        let d = UIView()
+        d.backgroundColor = .red
+        return d
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        setUpConstrains()
+    }
+    
+    func setUpConstrains() {
+        addSubview(divider)
+        divider.fillSuperview()
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
